eb8d521d3c6134614d36fa495ff508143f76b508
unknown <trejomsamuel@gmail.com>
2024-05-18 17:43:42 -0500

    added routing to category selection

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -6,15 +6,11 @@ import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-landing',
   template: `
+  <div class="container">
     <div class="d-flex flex-wrap justify-content-center mt-5">
       <div *ngFor="let taskGroup of taskGroups" class="card m-3" style="width: 18rem;">
-        <div class="card-body">
-          <!-- <h5 class="card-title"></h5> -->
+        <div class="card-body" style="cursor: pointer;" (click)="goToCategory(taskGroup.category)">
           <h6 class="card-subtitle mb-2 text-body-secondary">{{taskGroup.category}}</h6>
-          <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a> -->
-
           <div class="mute text-end me-2" style="font-size: 10px;">{{getNumTasksCompletedString(taskGroup)}}</div>
           <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
             <div class="progress-bar" style="width: {{getPercentageComplete(taskGroup)}}%"></div>
@@ -22,18 +18,16 @@ import { TaskService } from 'src/app/services/task.service';
         </div>
       </div>
     </div>
+  </div>
   `,
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
   taskGroups: TaskGroup[];
   taskService: TaskService = inject(TaskService);
-  activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   router: Router = inject(Router);
-  // category: string;
 
   ngOnInit() {
-    // this.category = this.activatedRoute.snapshot.params['category'];
     this.taskService.taskGroups$.subscribe(data => {
       this.taskGroups = data;
     });
@@ -54,5 +48,7 @@ export class LandingComponent {
     return completeTasksPercentage;
   }
 
-  
+  goToCategory(category: string) {
+    this.router.navigate(['/' + category]);
+  }
 }
