d41fedf589e2f8f54f00e283ed509b4df1c45176
Samuel Trejo <trejomsamuel@gmail.com>
2024-06-05 00:04:06 -0500

    style adjustments to flashcards, and adding starter flashcard english vocab data in constants

--- src/app/components/flashcards/flashcards.component.ts
+++ src/app/components/flashcards/flashcards.component.ts
@@ -4,25 +4,29 @@ import { DateService } from 'src/app/services/date.service';
 @Component({
   selector: 'app-flashcards',
   template: `
-  <div class="py-3 bg-stdark text-light">
-    <div class="container">
-      <h3>Overview</h3>
-      <div>{{headerDate}}</div>
-    </div>
-  </div>
+  <div style="overflow: hidden;">
 
+    <div class="py-3 bg-stdark text-light">
+      <div class="container">
+        <h3>Overview</h3>
+        <div>{{headerDate}}</div>
+      </div>
+    </div>
   
-  <div class="container mt-5">
-    <div class="flip-card border border-primary bg-stdark rounded">
-      <div class="flip-card-inner">
-        <div class="flip-card-front border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
-          <div class="text-light">test</div>
-        </div>
-        <div class="flip-card-back border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
-          <div class="text-light">lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum </div>
+    
+    <div class="container mt-5 mb-5">
+      <div class="flip-card bg-stdark rounded">
+        <div class="flip-card-inner">
+          <div class="flip-card-front border border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
+            <div class="text-light">test</div>
+          </div>
+          <div class="flip-card-back border border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
+            <div class="text-light">lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum </div>
+          </div>
         </div>
       </div>
     </div>
+
   </div>
   `,
   styleUrls: ['./flashcards.component.scss']
