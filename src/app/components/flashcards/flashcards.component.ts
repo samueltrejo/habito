97deef98de375393f05916d7372cbfc56bc2e8ef
Samuel Trejo <trejomsamuel@gmail.com>
2024-06-05 20:31:21 -0500

    test data flashcards showing and flipping on screen

--- src/app/components/flashcards/flashcards.component.ts
+++ src/app/components/flashcards/flashcards.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { DateService } from 'src/app/services/date.service';
+import { FLASH_CARDS } from 'src/app/constants/constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-flashcards',
@@ -14,17 +16,42 @@ import { DateService } from 'src/app/services/date.service';
     </div>
   
     
-    <div class="container mt-5 mb-5">
-      <div class="flip-card bg-stdark rounded">
+    <!-- <div class="container mt-5 mb-5">
+      <div *ngFor="let flashCard of flashCards" class="flip-card bg-stdark rounded">
         <div class="flip-card-inner">
           <div class="flip-card-front border border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
-            <div class="text-light">test</div>
+            <div class="text-light">{{flashCard.front}}</div>
           </div>
           <div class="flip-card-back border border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
-            <div class="text-light">lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum </div>
+            <div class="text-light">{{flashCard.back}}</div>
           </div>
         </div>
       </div>
+    </div> -->
+
+    <div class="container d-flex justify-content-center mt-5">
+      <div style="height: 300px; width: 100px; background-color: red" (click)="changeCard()"></div>
+      <div class="slider">
+        <!-- <a *ngFor="let flashCard of flashCards" href="flashcards#{{flashCard.id}}">{{flashCard.id}}</a> -->
+
+        <div class="slides">
+          <div *ngFor="let flashCard of flashCards" id="{{flashCard.id}}">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front border border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
+                  <div class="text-light">{{flashCard.front}}</div>
+                </div>
+                <div class="flip-card-back border border-primary bg-stdark rounded d-flex justify-content-center align-items-center">
+                  <div class="text-light">{{flashCard.back}}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="/flashcards#fs{{flashCardId}}" (click)="changeCard()">
+        <div style="height: 300px; width: 100px; background-color: red"></div>
+      </a>
     </div>
 
   </div>
@@ -33,6 +60,13 @@ import { DateService } from 'src/app/services/date.service';
 })
 export class FlashcardsComponent {
   dateService: DateService = inject(DateService);
+  router: Router = inject(Router);
   headerDate: string = this.dateService.getDateString();
+  flashCards: any = FLASH_CARDS;
+  flashCardId: number = 1;
 
+  changeCard() {
+    this.flashCardId++;
+    return this.flashCardId;
+  }
 }
